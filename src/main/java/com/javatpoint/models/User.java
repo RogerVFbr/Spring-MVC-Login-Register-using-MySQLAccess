e712be4a0802ed79e1b1275de9a0723b969fab9c
RogerVFbr <roger.freret@al.infnet.edu.br>
2019-09-12 17:31:04 -0300

    Clean commit.

--- src/main/java/com/javatpoint/models/User.java
+++ src/main/java/com/javatpoint/models/User.java
@@ -58,20 +58,4 @@ public class User {
                 ", email='" + email + '\'' +
                 '}';
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return userId == user.userId &&
-                Objects.equals(name, user.name) &&
-                Objects.equals(password, user.password) &&
-                Objects.equals(email, user.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(userId, name, password, email);
-    }
 }
